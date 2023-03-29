b711daa1d9e4fe031a838c0f47c3ae3d06ddf44c
remy <remyciterin@gmail.com>
2023-03-29 22:36:03 +0200

    Tactic.lean: add composition of Morphism

--- Category/Tactics.lean
+++ Category/Tactics.lean
@@ -16,44 +16,70 @@ structure Result {α:Q(Type u)} (E: Q($α) → Type) (e: Q($α)) where
   proof : Q($e = $expr)
 
 def Result.Id {α:Q(Type u)} {E:Q($α) → Type} (e: Q($α)) (val: E e) : Result E e where
-  expr := e
-  val  := val
+  expr  := e
+  val   := val
   proof := q(by rfl)
 
-namespace FiniteCat
+def Result.map {α:Q(Type u)} {E:Q($α) → Type} {e: Q($α)}
+  (r:Result E e) (F:Q($α) → Type) (f: E r.expr → F r.expr) : Result F e where
+  expr  := r.expr
+  val   := f r.val
+  proof := r.proof
 
+namespace Cat
+
+structure Context where
+  useTransparancy : Bool
+
+structure State where
+  atoms : Array Expr := #[]
+
+instance : Inhabited State where
+  default := ⟨#[]⟩
+
+abbrev CatM := ReaderT Context <| StateT State MetaM
+
+def CatM.run {α:Type} (f:CatM α) (red:Bool) :
+  MetaM α := (f ⟨red⟩).run' {}
+
+def CatM.add_atom (e:Expr) : CatM Nat := do
+  let table ← get
+  if (← read).useTransparancy then
+    for h : i in [0:table.atoms.size] do
+      have : i < table.atoms.size := h.2
+      if ← isDefEq e table.atoms[i] then
+        return i
+  modifyGet fun c => (table.atoms.size, {c with atoms := c.atoms.push e})
 
 section
 
 variable {C: Q(Type u)}
 variable {CatC: Q(Category.{u, v} $C)}
 
-inductive AtomicMorphism : ∀ X Y: Q($C), Q($X ⟶  $Y) → Type where
-| Const : ∀ X Y: Q($C), ∀ f: Q($X ⟶  $Y), Nat → AtomicMorphism X Y f
+inductive Atom : ∀ X Y: Q($C), Q($X ⟶  $Y) → Type where
+| Const : ∀ X Y: Q($C), ∀ f: Q($X ⟶  $Y), Nat → Atom X Y f
 
-inductive AtomicMorphismList : ∀ X Y: Q($C), Q($X ⟶  $Y) → Type where
-| Nil : ∀ {X Y f}, AtomicMorphism X Y f → AtomicMorphismList X Y f
+inductive AtomicMorphism : ∀ X Y: Q($C), Q($X ⟶  $Y) → Type where
+| Nil : ∀ {X Y f}, Atom X Y f → AtomicMorphism X Y f
 | Cons: ∀ {X Z: Q($C)} (Y:Q($C)),
   ∀ f: Q($Y ⟶  $Z), ∀ g: Q($X ⟶  $Y),
-  AtomicMorphism Y Z f → AtomicMorphismList X Y g → AtomicMorphismList X Z q($f ⊚ $g)
---| RW : ∀ {X Y:Q($C)}, ∀ f g:Q($X ⟶  $Y), Q($f = $g) → AtomicMorphismList X Y f → AtomicMorphismList X Y g
+  Atom Y Z f → AtomicMorphism X Y g → AtomicMorphism X Z q($f ⊚ $g)
 
 
-inductive MorphismList : ∀ X Y:Q($C), Q($X ⟶  $Y) → Type where
-| Id   : ∀ X: Q($C), MorphismList X X q(𝟙 $X)
-| List : ∀ {X Y}, ∀ f, AtomicMorphismList X Y f → MorphismList X Y f
-
+inductive Morphism : ∀ X Y:Q($C), Q($X ⟶  $Y) → Type where
+| Id   : ∀ X: Q($C), Morphism X X q(𝟙 $X)
+| List : ∀ {X Y}, ∀ f, AtomicMorphism X Y f → Morphism X Y f
 
 #check Result
-#check AtomicMorphismList
+#check AtomicMorphism
 
 
-def AtomMorphismList.compose {X Y Z: Q($C)} (f1:Q($Y ⟶  $Z)) (f2:Q($X ⟶  $Y))
-  (l1:AtomicMorphismList Y Z f1) (l2:AtomicMorphismList X Y f2) :
-    @Result _ q($X ⟶  $Z) (@AtomicMorphismList _ _ C CatC X Z) q($f1 ⊚ $f2) :=
+def AtomicMorphism.compose {X Y Z: Q($C)} (f1:Q($Y ⟶  $Z)) (f2:Q($X ⟶  $Y))
+  (l1:AtomicMorphism Y Z f1) (l2:AtomicMorphism X Y f2) :
+    @Result _ q($X ⟶  $Z) (@AtomicMorphism _ _ C CatC X Z) q($f1 ⊚ $f2) :=
 
   match l1 with
-  | AtomicMorphismList.Nil atom => Result.Id q($f1 ⊚ $f2) (.Cons Y f1 f2 atom l2)
+  | AtomicMorphism.Nil atom => Result.Id q($f1 ⊚ $f2) (.Cons Y f1 f2 atom l2)
   | .Cons Z f1 g1 atom l1 =>
   by
     let r := compose g1 f2 l1 l2
@@ -70,20 +96,37 @@ def AtomMorphismList.compose {X Y Z: Q($C)} (f1:Q($Y ⟶  $Z)) (f2:Q($X ⟶  $Y)
     }
 
 
+def Morphism.compose {X Y Z: Q($C)} (f1: Q($Y ⟶  $Z)) (f2: Q($X ⟶  $Y))
+  (l1: @Morphism _ _ C CatC Y Z f1) (l2: @Morphism _ _ C CatC X Y f2) :
+  @Result _ q($X ⟶  $Z) (@Morphism _ _ C CatC X Z) q($f1 ⊚ $f2) :=
+by
+  cases l1 with
+  | Id =>
+    apply Result.mk
+    case expr =>
+      exact f2
+    case val =>
+      exact l2
+    case proof =>
+      exact q(by
+        rw [Category.id_comp]
+      )
+  | List _ a =>
+    cases l2 with
+    | Id =>
+      apply Result.mk
+      case expr =>
+        exact f1
+      case val =>
+        exact (.List _ a)
+      case proof =>
+        exact q(by
+          rw [Category.comp_id]
+        )
+    | List _ b =>
+      apply Result.map (AtomicMorphism.compose f1 f2 a b)
+      apply Morphism.List
+
+
 end
-
-structure Context where
-  useTransparancy : Bool
-
-structure State where
-  atoms : Array Expr := #[]
-
-abbrev FiniteCatM := ReaderT Context <| StateT State MetaM
-
-def FiniteCatM.run {α:Type} (f:FiniteCatM α) (red:Bool) :
-  MetaM α := (f ⟨red⟩).run' {}
-
-
-
-
-end FiniteCat
+end Cat
